82585f32a6139bc46dcc84dba034b8581e6ff5e8
João Paulo Fontana Macedo <jpfontana12@icloud.com>
2024-06-18 20:52:14 -0300

    feat(internal/users/list_test.go): added update user test

---
+++ internal/users/modify_test.go
@@ -0,0 +1,34 @@
+package users
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+)
+
+func TestUpdate(t *testing.T) {
+	db, mock, err := sqlmock.New()
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	defer db.Close()
+
+	mock.ExpectExec(regexp.QuoteMeta(`update "users" set "name"=$1, "modified_at"=$2 where id=$3`)).
+		WithArgs("João", AnyTime{}, 1).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	err = Update(db, 1, &User{Name: "João"})
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = mock.ExpectationsWereMet()
+
+	if err != nil {
+		t.Error(err)
+	}
+}
